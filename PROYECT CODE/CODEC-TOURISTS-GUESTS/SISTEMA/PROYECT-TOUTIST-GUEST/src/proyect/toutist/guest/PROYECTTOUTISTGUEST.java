632693597eb9f9dea7dec054e515fc8b737479a5
Marlon <mjortiz10@espe.edu.ec>
2021-06-21 20:16:55 -0500

    update

---
+++ PROYECT CODE/CODEC-TOURISTS-GUESTS/SISTEMA/PROYECT-TOUTIST-GUEST/src/proyect/toutist/guest/PROYECTTOUTISTGUEST.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyect.toutist.guest;
+
+/**
+ *
+ * @author Marlon Ortiz Codec ESPE-DCCO
+ */
+public class PROYECTTOUTISTGUEST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Ya muchachos");
+    }
+    
+}
