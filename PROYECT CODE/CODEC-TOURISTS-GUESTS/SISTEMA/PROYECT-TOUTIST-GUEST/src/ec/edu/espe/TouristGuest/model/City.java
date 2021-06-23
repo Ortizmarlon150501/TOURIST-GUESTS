b8f5cc682dd79883defee3cedcca600d238bc579
LilianIza <Leiza1@espe.edu.ec>
2021-06-22 22:38:35 -0500

    Codec

--- PROYECT CODE/CODEC-TOURISTS-GUESTS/SISTEMA/PROYECT-TOUTIST-GUEST/src/ec/edu/espe/TouristGuest/model/City.java
+++ PROYECT CODE/CODEC-TOURISTS-GUESTS/SISTEMA/PROYECT-TOUTIST-GUEST/src/ec/edu/espe/TouristGuest/model/City.java
@@ -11,5 +11,58 @@ package ec.edu.espe.TouristGuest.model;
  */
 public class City {
     
+    private String name;
+    private int numberOfUniversitys;
+    private int amountOfMuseum;
+
+    public City(String name, int numberOfUniversitys, int amountOfMuseum) {
+        this.name = name;
+        this.numberOfUniversitys = numberOfUniversitys;
+        this.amountOfMuseum = amountOfMuseum;
+    }
+  
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the numberOfUniversitys
+     */
+    public int getNumberOfUniversitys() {
+        return numberOfUniversitys;
+    }
+
+    /**
+     * @param numberOfUniversitys the numberOfUniversitys to set
+     */
+    public void setNumberOfUniversitys(int numberOfUniversitys) {
+        this.numberOfUniversitys = numberOfUniversitys;
+    }
+
+    /**
+     * @return the amountOfMuseum
+     */
+    public int getAmountOfMuseum() {
+        return amountOfMuseum;
+    }
+
+    /**
+     * @param amountOfMuseum the amountOfMuseum to set
+     */
+    public void setAmountOfMuseum(int amountOfMuseum) {
+        this.amountOfMuseum = amountOfMuseum;
+    }
+    
+    
     
 }
