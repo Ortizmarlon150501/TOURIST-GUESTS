6009c6d46696ab5803b8299a319595d02243218f
KerlyMosquera <kemosquera1@espe.edu.ec>
2021-08-04 07:09:26 -0500

    corrections

--- 6.Code/PROYECT-TOURIST-GUEST/src/ec/edu/espe/TouristGuest/view/Register_Form.java
+++ 6.Code/PROYECT-TOURIST-GUEST/src/ec/edu/espe/TouristGuest/view/Register_Form.java
@@ -404,7 +404,7 @@ public class Register_Form extends javax.swing.JFrame {
     }//GEN-LAST:event_btnSave_RegiActionPerformed
 
     private void btnDelete_RegiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDelete_RegiActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_btnDelete_RegiActionPerformed
 
     int filas;
