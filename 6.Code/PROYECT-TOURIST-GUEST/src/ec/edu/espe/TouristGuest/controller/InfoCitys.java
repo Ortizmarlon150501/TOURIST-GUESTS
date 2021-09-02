51110f12cc17f619adb0769b69b44b39592870b5
Marlon <mjortiz10@espe.edu.ec>
2021-09-02 00:18:50 -0500

    InfoCityClass

---
+++ 6.Code/PROYECT-TOURIST-GUEST/src/ec/edu/espe/TouristGuest/controller/InfoCitys.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.TouristGuest.controller;
+
+/**
+ *
+ * @author Marlon Ortiz Codec ESPE-DCCO
+ */
+public class InfoCitys {
+
+    private String name;
+    private double numberOfInHabitants;
+    private String idiom;
+    private String continent;
+    private int numberOfUniversity;
+    private int numberOfMuseum;
+
+    
+    
+}
